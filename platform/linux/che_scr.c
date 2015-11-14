45874f6e1e9bd4419ac7a28c528e64f592f2dd2c
Igor Eslava <netrisk@gmail.com>
2015-11-14 21:32:31 +0100

    Add basic console screen
    Implementation of draw_sprite function has also been fixed.

--- platform/linux/che_scr.c
+++ platform/linux/che_scr.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 
+#define CHE_SCR_TEST
+
 int che_scr_init(che_scr_t *s, int width, int height)
 {
 	if (width % 8)
@@ -14,8 +16,7 @@ int che_scr_init(che_scr_t *s, int width, int height)
 	return 0;
 }
 
-/* TODO: Test this code, it's completely untested */
-/* TODO: Maybe this function can general code, not platform code */
+/* TODO: Maybe this function can be in general code, not platform code */
 bool che_scr_draw_sprite(che_scr_t *s, uint8_t *buf, int h, int x, int y)
 {
 	uint8_t *spr_ptr;
@@ -24,9 +25,9 @@ bool che_scr_draw_sprite(che_scr_t *s, uint8_t *buf, int h, int x, int y)
 	int i;
 
 	/* Wrap coordinates around the screen */
-	if (x > s->w)
+	if (x >= s->w)
 		x = x % s->w;
-	if (y > s->h)
+	if (y >= s->h)
 		y = y % s->h;
 
 	/* Cut the sprite if necessary */
@@ -48,13 +49,26 @@ bool che_scr_draw_sprite(che_scr_t *s, uint8_t *buf, int h, int x, int y)
 	/* Draw content in memory */
 	bool collision = false;
 	for (i = 0; i < h_real; i++) {
-		int pos = s->w * (y + i) + x;
-		uint8_t xor_byte = s->data[pos] ^ spr_ptr[i];
+		int rsh = x & 7;
+		int pos = s->w * (y + i) + (x >> 3);
+		uint8_t spr_byte = (spr_ptr[i] >> rsh);
+		uint8_t xor_byte = s->data[pos] ^ spr_byte;
+			
 		if (!collision) {
-			uint8_t or_byte = s->data[pos] | spr_ptr[i];
+			uint8_t or_byte = s->data[pos] | spr_byte;
 			collision = or_byte != xor_byte;
 		}
 		s->data[pos] = xor_byte;
+		if (!x_cut && rsh) {
+			int lsh = 8 - rsh;
+			spr_byte = (spr_ptr[i] << lsh);
+			xor_byte = s->data[pos + 1] ^ spr_byte;
+			if (!collision) {
+				uint8_t or_byte = s->data[pos + 1] | spr_byte;
+				collision = or_byte != xor_byte;
+			}
+			s->data[pos + 1] = xor_byte;
+		}
 	}
 
 	s->changed = true;
@@ -65,14 +79,41 @@ void che_scr_clear(che_scr_t *s)
 {
 	memset(s->data, 0, (s->w >> 3) * s->h);
 	s->changed = true;
+
+	#ifdef CHE_SCR_TEST
+	uint8_t sprite = 0xff;
+	int x;
+	for (x = 0; x < s->w; x++)
+		che_scr_draw_sprite(s, &sprite, 1, x, x);
+	#endif
 }
 
 void che_scr_draw_screen(che_scr_t *s)
 {
+	int x, y;
+
 	if (!s->changed)
 		return;
 	s->changed = false;
-	printf("Screen changed\n");
+	
+	/* Clear the screen */
+	printf("\e[1;1H\e[2J\n");
+
+	for (y = 0; y < s->h; y++) {
+		char line[256];
+		int pos = 0;
+		for (x = 0; x < s->w; x++) {
+			char ch;
+			if ((s->data[y * s->w + (x >> 3)] >> (7 - (x & 7))) & 1)
+				ch = 'O';
+			else
+				ch = '.';
+			line[pos++] = ch;
+		}
+		line[pos] = 0;
+		printf("%s\n", line);
+	}
+	fflush(stdout);
 }
 
 void che_scr_end(che_scr_t *s)
