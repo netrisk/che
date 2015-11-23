acf0cdeeea776f6399142a21ebc5c19169591229
Igor Eslava <netrisk@gmail.com>
2015-11-23 19:25:41 +0100

    Remove wrongly commited dependency files

--- src/che_scr.d
+++
@@ -1,6 +0,0 @@
-src/che_scr.o: src/che_scr.c src/che_scr.h \
- src/platform/linux/che_scr_platform.h
-
-src/che_scr.h:
-
-src/platform/linux/che_scr_platform.h:
--- src/che_time.d
+++
@@ -1,3 +0,0 @@
-src/che_time.o: src/che_time.c src/che_time.h
-
-src/che_time.h:
