ffdf8eb3e8e73f8564180e493b6f1cca9119ad74
Igor Eslava <netrisk@gmail.com>
2015-11-23 21:25:11 +0100

    Fix header flags

--- src/che_util.h
+++ src/che_util.h
@@ -1,8 +1,9 @@
 #ifndef CHE_UTIL_H
+#define CHE_UTIL_H
 
 #include <stddef.h>
 #define che_containerof(ptr, type, member) ({                \
         const typeof( ((type *)0)->member ) *__mptr = (ptr); \
         (type *)( (char *)__mptr - offsetof(type,member) );})
 
-#endif
+#endif /* CHE_UTIL_H */
