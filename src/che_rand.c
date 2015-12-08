2bf9e036c8ee80d308fb6497414450b7fe69a486
Igor Eslava <netrisk@gmail.com>
2015-12-08 09:10:05 +0100

    Add a TODO for platform independency

--- src/che_rand.c
+++ src/che_rand.c
@@ -1,6 +1,8 @@
 #include <che_rand.h>
 #include <stdlib.h>
 
+/* TODO: This is just for PC, make a platform independent random */
+
 int che_rand_init(che_rand_t *r, unsigned int seed)
 {
 	return 0;
